1f3357f0918183de82baacb186515202dab7d6bd
Maurice Aney <mauriceaneyy@gmail.com>
2022-08-29 11:28:01 +0200

    allowed cross origin communication between backend and frontend

--- src/main/java/com/fsdl/awsimageuploader/profile/UserProfileController.java
+++ src/main/java/com/fsdl/awsimageuploader/profile/UserProfileController.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "api/v1/user-profile")
+@CrossOrigin("*")
 public class UserProfileController {
 
     private final UserProfileService userProfileService;
